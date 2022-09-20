47c51e188e387cfe09b7a35029ecc4b9ec061e06
FatimaSaba <113553379+FatimaSaba@users.noreply.github.com>
2022-09-20 16:42:37 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cafe</title>
+    <link href="style.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+    <a href="https://img.freepik.com/premium-vector/coffee-logo-design_7169-78.jpg?w=826" rel="logo">Cofftail</a>
+    </header>
+    <nav class="nav">
+       <a href="home">Home</a>
+       <a href="gallery">Gallery</a>
+       <a href="speciality">Speciality</a>
+       <a href="order">Order</a>
+       <a href="review">Review</a>
+
+
+        <?php
+            echo '<a href="review">this was created from php</a>';
+
+        ?>
+
+
+
+    </nav>
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="style.js"></script>
+</body>
+</html>
