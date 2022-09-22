d3ae8c112411002cf340f8367a52f68c18eed84c
FatimaSaba <sabafatima5478@gmail.com>
2022-09-22 18:39:43 +0300

    i done some work in section

--- index.php
+++ index.php
@@ -8,38 +8,119 @@
     <link href="style.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet">
 </head>
-<body>
+<body style="margin-top:220px">
     <header>
-    <a href="https://img.freepik.com/premium-vector/coffee-logo-design_7169-78.jpg?w=826" rel="logo">Cofftail</a>
-    </header>
-    <nav class="nav">
-       <a href="home">Home</a>
-       <a href="gallery">Gallery</a>
-       <a href="speciality">Speciality</a>
-       <a href="order">Order</a>
-       <a href="review">Review</a>
 
+       <img src= "./resources/img/cafe-logo.png"  width="150" height="150" alt="img">
 
         <?php
-            echo '<a href="review">this was created from php</a>';
-
+       echo "<h3><em>Cafetail</em><h3>";
         ?>
 
+      <nav class="nav">
+          <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#gallery">Gallery</a></li>
+              <li><a href="#speciality">Speciality</a></li>
+              <li><a href="#order">Order</a></li>
+              <li><a href="#review">Review</a></li>
+          </ul>
+      </nav>
+       
+    </header>
+<section class="container1">
+        
+    <div class="split-left-img" >
+     <img src="./resources/img/home.jpeg" alt="coffee image" wight="200" height="600">
+    </div>
+
+    <div class="split-right-para">
+    <p>
+    We are the leading Instagrammable café and lifestyle brand. Everything that we make is irresistibly chic, pink perfection. Our products help our customers create that picture perfect moment – every time. 
+    </p>
+
+    </div>
+    <div class="sticker">
+    <img src="./resources/img/sticker.png" alt="coffee bean" weight="30" height="30">
+    </div>
+
+</section>
+<section class="container2">
+    <div class="Gallery">
+     <h1><span>Gallery</span></h1>
+    </div>
+     <img src=" alt=">
 
 
-    </nav>
- 
 
+</section>
+<section class="container3">
+<div class="speciality">
+    <h1 class= "heading"><span>Speciality</span></h2>
+</div>
+<div class="boxes">
+    <img src="./resources/img/imagesf.jpeg" alt="flat white"></div>
+    <div class="text">
+        <h3>Flat White</h3>
 
+    <p> The flat white coffee is an espresso-based coffee drink accompanied with steamed milk and microfoam. This microfoam is made up of steamed milk which is gently infused with air. This results in silky, textured milk containing tiny air bubbles.</p>
+</div>
+</div>
 
+<div class="boxes">
+    <img src="cold-coffee-with-ice-cream-1.webp" alt="Cold-Coffee with Ice-cream"></div>
+    <div class="text">
+        <h3>Flat White</h3>
 
+    <p> Cold Coffee with Ice Cream is a perfect treat for all coffee lovers. Made with only 3 ingredients and ready in 5 minutes it's a great way to cool down during the summer! Also try this Mango Lassi, perfect for hot days!
+</div>
+</div>
+<div class="boxes">
+    <img src="imagesi.jpeg" alt="Capppuccino"></div>
+    <div class="text">
+        <h3>Capppuccino</h3>
 
+    <p> A cappuccino is an espresso-based coffee drink that originated in Italy and is prepared with steamed milk foam. 
+</div>
+</div>
+<div class="boxes">
+    <img src="imagesd.jpeg" alt="Espresso"></div>
+    <div class="text">
+        <h3>Espresso</h3>
 
+    <p> An espresso is intense and bold, but nonetheless delicious, and the espresso is usually served in small demitasse-style cups for this reason.
+</div>
+</div>
+<div class="boxes">
+    <img src="imagesa.jpeg" alt="Macchiato"></div>
+    <div class="text">
+        <h3>Macchiato</h3>
 
+    <p> n Italian, the term "macchiato" translates as "marked" or "stained", meaning a stained or marked coffee. The macchiato is an espresso coffee drink, topped with a small amount of foamed or steamed milk to allow the taste of the espresso to still shine through.
+</div>
+</div>
+<div class="boxes">
+    <img src="imagesj.jpeg" alt="Frappe"></div>
+    <div class="text">
+        <h3>Frappe</h3>
 
+    <p> A frappé coffee, Greek frappé, Nescafé frappé, or just frappé is a Greek iced coffee drink made from instant coffee, water, sugar, and milk. 
+</div>
+</div>
+<div class="boxes">
+    <img src="Caramel-Macchiato-1.jpeg" alt= "Caramel Macchiato"></div>
+    <div class="text">
+        <h3>Caramel Macchiato"</h3>
 
+    <p> A Caramel Macchiato is a coffee beverage with steamed milk, espresso, vanilla syrup and caramel drizzle. It’s so delicious!
+</div> 
+</div>
+    
+    
+    
+    <div>
 
-
+    
 
 
 
