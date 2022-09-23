aaf430b7202ff61d438d5e37d310a8158715085b
FatimaSaba <sabafatima5478@gmail.com>
2022-09-24 00:47:10 +0300

    changes

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
       <nav class="nav">
           <ul>
               <li><a href="#home">Home</a></li>
-              <li><a href="#gallery">Gallery</a></li>
+              <li><a href="#Menu">Menu</a></li>
               <li><a href="#speciality">Speciality</a></li>
               <li><a href="#order">Order</a></li>
               <li><a href="#review">Review</a></li>
@@ -46,16 +46,174 @@
 
 </section>
 <section class="container2">
-    <div class="Gallery">
-     <h1><span>Gallery</span></h1>
+    <div class="Menu" id="Menu">
+     <h1><span>Menu</span></h1>
     </div>
-     <img src=" alt=">
+    <div class="mboxes">
+        <span class="price">$7</span>
+    <img src="./resources/img/imagesf.jpeg" alt="flat white"></div>
+    <div class="text">
+        <h3>Flat White</h3>
+<div class="stars">
+
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+
+</div>
+<a href="#" class="btn">order now</a>
+</div>
+<div class="mboxes">
+        <span class="price">$8</span>
+    <img src="./resources/img/Macchiato.jpg" alt="Macchiato"></div>
+    <div class="text">
+        <h3>Macchiato</h3>
+<div class="stars">
+
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+
+</div>
+<a href="#" class="btn">order now</a>
+</div>
+<div class="mboxes">
+        <span class="price">$5</span>
+    <img src="./resources/img/cafe mocha.jpg" alt="cafe mocha"></div>
+    <div class="text">
+        <h3>Cafe Mocha</h3>
+<div class="stars">
+
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+
+</div>
+<a href="#" class="btn">order now</a>
+</div>
+<div class="mboxes">
+        <span class="price">$4</span>
+    <img src="./resources/img/espresso.webp" alt="Espresso"></div>
+    <div class="text">
+        <h3>Espresso</h3>
+<div class="stars">
+
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+
+</div>
+<a href="#" class="btn">order now</a>
+</div>
+<div class="mboxes">
+        <span class="price">$7</span>
+    <img src="./resources/img/imagesi.jpeg" alt="Capppuccino"></div>
+    <div class="text">
+        <h3>Capppuccino</h3>
+<div class="stars">
+
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+
+</div>
+<a href="#" class="btn">order now</a>
+</div>
+<div class="mboxes">
+        <span class="price">$4</span>
+    <img src="./resources/img/americano.jpeg" alt="americano"></div>
+    <div class="text">
+        <h3>Americano</h3>
+<div class="stars">
+
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+
+</div>
+<a href="#" class="btn">order now</a>
+</div>
+<div class="mboxes">
+        <span class="price">$8</span>
+    <img src="./resources/img/irish.jpg" alt="Irish Coffee"></div>
+    <div class="text">
+        <h3>Irish Coffee</h3>
+<div class="stars">
+
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+
+</div>
+<a href="#" class="btn">order now</a>
+</div>
+<div class="mboxes">
+        <span class="price">$10</span>
+    <img src="./resources/img/coldcoffeewithicecream.jpg" alt="Cold-Coffee with Ice-cream"></div>
+    <div class="text">
+        <h3>Cold-Coffee with Ice-cream</h3>
+<div class="stars">
+
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+
+</div>
+<a href="#" class="btn">order now</a>
+</div>
+<div class="mboxes">
+        <span class="price">$8</span>
+    <img src="./resources/img/latte.jpg" alt="Latte"></div>
+    <div class="text">
+        <h3>Latte</h3>
+<div class="stars">
+
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+
+</div>
+<a href="#" class="btn">order now</a>
+</div>
+<div class="mboxes">
+        <span class="price">$10</span>
+    <img src="./resources/img/frappe.jpg" alt="Frappe"></div>
+    <div class="text">
+        <h3>Frappe</h3>
+
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+<i class="fas fa star"></i>
+
+</div>
+<a href="#" class="btn">order now</a>
+</div>
 
 
 
 </section>
 <section class="container3">
-<div class="speciality">
+<div class="speciality" id="speciality">
     <h1 class= "heading"><span>Speciality</span></h2>
 </div>
 <div class="boxes">
@@ -68,7 +226,7 @@
 </div>
 
 <div class="boxes">
-    <img src="cold-coffee-with-ice-cream-1.webp" alt="Cold-Coffee with Ice-cream"></div>
+    <img src="./resources/img/cold-coffee-with-ice-cream-1.webp" alt="Cold-Coffee with Ice-cream"></div>
     <div class="text">
         <h3>Flat White</h3>
 
@@ -76,7 +234,7 @@
 </div>
 </div>
 <div class="boxes">
-    <img src="imagesi.jpeg" alt="Capppuccino"></div>
+    <img src="./resources/img/imagesi.jpeg" alt="Capppuccino"></div>
     <div class="text">
         <h3>Capppuccino</h3>
 
@@ -84,7 +242,7 @@
 </div>
 </div>
 <div class="boxes">
-    <img src="imagesd.jpeg" alt="Espresso"></div>
+    <img src="./resources/img/imagesd.jpeg" alt="Espresso"></div>
     <div class="text">
         <h3>Espresso</h3>
 
@@ -92,7 +250,7 @@
 </div>
 </div>
 <div class="boxes">
-    <img src="imagesa.jpeg" alt="Macchiato"></div>
+    <img src="./resources/img/imagesa.jpeg" alt="Macchiato"></div>
     <div class="text">
         <h3>Macchiato</h3>
 
@@ -100,7 +258,7 @@
 </div>
 </div>
 <div class="boxes">
-    <img src="imagesj.jpeg" alt="Frappe"></div>
+    <img src="./resources/img/imagesj.jpeg" alt="Frappe"></div>
     <div class="text">
         <h3>Frappe</h3>
 
@@ -108,7 +266,7 @@
 </div>
 </div>
 <div class="boxes">
-    <img src="Caramel-Macchiato-1.jpeg" alt= "Caramel Macchiato"></div>
+    <img src="./resources/img/Caramel-Macchiato-1.jpeg" alt= "Caramel Macchiato"></div>
     <div class="text">
         <h3>Caramel Macchiato"</h3>
 
