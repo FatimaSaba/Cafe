6fa039b8e8f1e431567d30bd69cc02802a794886
David <doskius200@gmail.com>
2022-09-24 01:35:19 +0300

    Refactoring

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cafe</title>
     <link href="style.css" rel="stylesheet">
-    <link href="style_d.css" rel="stylesheet">
+    <link href="style_header.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet">
 </head>
 
